9ed6646c48a448f9761a1ebe0f865ecf50bd11c2
kausikkris10@gmail.com <kausikkris10@gmail.com>
2019-10-27 17:18:38 -0700

    Added view image feed script

---
+++ src/enph353/enph353_utils/scripts/view_image.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+roslib.load_manifest('enph353_utils')
+import sys
+import rospy
+import cv2
+import numpy as np
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw", Image, self.callback)
+
+  def callback(self,data):
+    try:
+      frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
+    cv2.imshow("Robot Camera", frame)
+    cv2.waitKey(1)
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
