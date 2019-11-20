e3d019901ca6466063c92245ea94977ee5bfef94
Kausik Krishnakumar <kausikkris10@gmail.com>
2019-11-19 20:42:09 -0800

    Improved outer straight, count parked cars and stop at inner turn

--- src/controller/src/controller.py
+++ src/controller/src/controller.py
@@ -28,50 +28,57 @@ INIT_TURN2 = 3
 ALIGN = 4
 STRAIGHT = 5
 PEDESTRIAN = 6
-LEAVE_RED = LEAVE_ZEBRA = 7
-INNER_TURN = 8
-INNER_STRAIGHT = 9
+LEAVE_ZEBRA = 7
+INNER_STRAIGHT = 8
 STOP = 100
 
-SCAN_Y1 = 580
-SCAN_YBLUE = 470
-SCAN_XBLUE = 200
+SCAN_LINE = 580
 SCAN_YZEBRA = [710, 715, 718]
+SCAN_XZEBRA = [200, 1080]
 SCAN_YPED = [379, 391, 403, 415]
+SCAN_XPED = [490, 890]
+SCAN_YBLUE = 516
+SCAN_XBLUE = 100
 LINE_FOLLOWX = 960
-LINE_THRESH = 5000
 TURN_THRESH = 50
 ZEBRA_THRESH = 250000
 PED_THRESH = 22000
-ZEBRA_WAIT_FRAMES = 10
+PARKED_THRESH = 10000
+PARKED_WAIT_FRAMES = 30
+PARKED_COUNT_END = 6
+ZEBRA_WAIT_FRAMES = 12
 
 class controller:
 
-  def __init__(self):
+  def __init__(self, state=INIT):
     self.bridge = CvBridge()
     self.vel_pub = rospy.Publisher(VEL_TOPIC, Twist, queue_size=30)
     self.image_sub = rospy.Subscriber(IMAGE_TOPIC, Image, self.callback)
     self.xaxes = np.indices((1, WIDTH))[1]
-    self.follow_state = INIT
+    self.follow_state = state
+    self.prevx = -1
+    self.prevy = -1
     cv2.namedWindow('cam', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('cam', WIDTH/2, HEIGHT/2)
-    def click_cb(event, x, y, flags, param):
-      if event == cv2.EVENT_LBUTTONDBLCLK:
-        print(x, y)
-    cv2.setMouseCallback('cam', click_cb)
+    cv2.setMouseCallback('cam', self.click_cb)
 
   def callback(self, data):
     try:
-      frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      image_raw = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame = cv2.cvtColor(image_raw, cv2.COLOR_BGR2HSV)
+    if self.prevx != -1:
+      print(self.prevy, self.prevx, frame[self.prevy, self.prevx])
+      self.prevx = -1
+      self.prevy = -1
 
     white_mask = cv2.inRange(frame, np.array([0,0,100]), np.array([255,0,255]))
+    blue_mask = cv2.inRange(frame, np.array([110,120,95]), np.array([130,255,210]))
 
     left = -1
     for i in range(0, WIDTH):
-      if white_mask[SCAN_Y1, i] == 255 and left == -1:
+      if white_mask[SCAN_LINE, i] == 255 and left == -1:
         left = i
       elif left != -1:
         left = (i + left)/2
@@ -79,7 +86,7 @@ class controller:
 
     right = -1
     for i in range(WIDTH - 1, 0, -1):
-      if white_mask[SCAN_Y1, i] == 255 and right == -1:
+      if white_mask[SCAN_LINE, i] == 255 and right == -1:
         right = i
       elif right != -1:
         right = (i + right)/2
@@ -88,6 +95,8 @@ class controller:
     if self.follow_state == INIT:
       self.wait = 0
       self.prev_ped = 0
+      self.prev_parked = 0
+      self.parked_count = 0
       self.send_vel(1, 0)
       print('forward')
       self.follow_state = INIT_FORWARD
@@ -108,7 +117,19 @@ class controller:
     elif self.follow_state == STRAIGHT:
       zebra_count = 0
       for i in SCAN_YZEBRA:
-        zebra_count += np.sum(white_mask[i,200:1080])
+        zebra_count += np.sum(white_mask[i,SCAN_XZEBRA[0]:SCAN_XZEBRA[1]])
+      blue_count = np.sum(blue_mask[SCAN_YBLUE,0:SCAN_XBLUE])
+      if blue_count > PARKED_THRESH and not self.prev_parked:
+        self.prev_parked += 1
+      elif self.prev_parked > PARKED_WAIT_FRAMES:
+        self.prev_parked = 0
+        self.parked_count += 1
+        print(self.parked_count)
+      elif self.prev_parked > 0:
+        self.prev_parked += 1
+      if self.parked_count >= PARKED_COUNT_END:
+        print('inner straight')
+        self.follow_state = INNER_STRAIGHT
       if zebra_count > ZEBRA_THRESH:
         self.send_vel(0, 0)
         print('ped')
@@ -116,15 +137,14 @@ class controller:
       elif np.abs(right - LINE_FOLLOWX) > TURN_THRESH:
         self.send_vel(0, 1 if LINE_FOLLOWX - right > 0 else -1)
       else:
-        self.send_vel(1, 0)
+        self.send_vel(1, 0)    
     elif self.follow_state == PEDESTRIAN:
       ped_count = 0
       for i in SCAN_YPED:
-        ped_count += np.sum(white_mask[i,490:890])
-      print(ped_count)
+        ped_count += np.sum(white_mask[i,SCAN_XPED[0]:SCAN_XPED[1]])
       if ped_count > PED_THRESH:
         self.prev_ped += 1
-      elif self.prev_ped  > 0:
+      elif self.prev_ped > 2:
         if self.wait > ZEBRA_WAIT_FRAMES:
           print('leave zebra')
           self.follow_state = LEAVE_ZEBRA
@@ -144,17 +164,25 @@ class controller:
         self.send_vel(0, 1 if LINE_FOLLOWX - right > 0 else -1)
       else:
         self.send_vel(1, 0)
-    elif self.follow_state == INNER_TURN:
-      pass
+    elif self.follow_state == INNER_STRAIGHT:
+      self.send_vel(0, 0)
     elif self.follow_state == STOP:
       self.send_vel(0, 0)
       self.follow_state += 1
+
+    cv2.imshow('cam', image_raw)
+    cv2.waitKey(1)
 
   def send_vel(self, lin, ang):
     velocity = Twist()
     velocity.linear.x = lin
     velocity.angular.z = ang
     self.vel_pub.publish(velocity)
+
+  def click_cb(self, event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+      self.prevx = x
+      self.prevy = y
 
 def main(args):
   ic = controller()
