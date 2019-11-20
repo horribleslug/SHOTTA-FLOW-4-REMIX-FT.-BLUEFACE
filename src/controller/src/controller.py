ab6a51f0c096fb0bf0335b8c310e9a387fad453f
Kausik Krishnakumar <kausikkris10@gmail.com>
2019-11-20 01:34:54 -0800

    Added incomplete inner ring following

--- src/controller/src/controller.py
+++ src/controller/src/controller.py
@@ -29,7 +29,8 @@ ALIGN = 4
 STRAIGHT = 5
 PEDESTRIAN = 6
 LEAVE_ZEBRA = 7
-INNER_STRAIGHT = 8
+INNER_TURN = 8
+INNER_STRAIGHT = 9
 STOP = 100
 
 SCAN_LINE = 580
@@ -40,13 +41,15 @@ SCAN_XPED = [490, 890]
 SCAN_YBLUE = 516
 SCAN_XBLUE = 100
 LINE_FOLLOWX = 960
-TURN_THRESH = 50
+INNER_FOLLOWX = 280
+TURN_THRESH = 55
 ZEBRA_THRESH = 250000
 PED_THRESH = 22000
 PARKED_THRESH = 10000
-PARKED_WAIT_FRAMES = 30
 PARKED_COUNT_END = 6
-ZEBRA_WAIT_FRAMES = 12
+PARKED_WAIT_FRAMES = 12
+PED_WAIT_FRAMES = 15
+INNER_TURN_MIN_FRAMES = 50
 
 class controller:
 
@@ -85,18 +88,26 @@ class controller:
         break
 
     right = -1
-    for i in range(WIDTH - 1, 0, -1):
-      if white_mask[SCAN_LINE, i] == 255 and right == -1:
-        right = i
-      elif right != -1:
-        right = (i + right)/2
-        break
+    if self.follow_state < INNER_STRAIGHT:
+      for i in range(WIDTH - 1, 0, -1):
+        if white_mask[SCAN_LINE, i] == 255 and right == -1:
+          right = i
+        elif right != -1:
+          right = (i + right)/2
+          break
+    else:
+      for i in range(int(WIDTH * 0.6), WIDTH):
+        if white_mask[SCAN_LINE, i] == 255 and right == -1:
+          right = i
+        elif right != -1:
+          right = (i + right)/2
+          break
 
     if self.follow_state == INIT:
       self.wait = 0
       self.prev_ped = 0
       self.prev_parked = 0
-      self.parked_count = 0
+      self.parked_count = 4
       self.send_vel(1, 0)
       print('forward')
       self.follow_state = INIT_FORWARD
@@ -119,8 +130,8 @@ class controller:
       for i in SCAN_YZEBRA:
         zebra_count += np.sum(white_mask[i,SCAN_XZEBRA[0]:SCAN_XZEBRA[1]])
       blue_count = np.sum(blue_mask[SCAN_YBLUE,0:SCAN_XBLUE])
-      if blue_count > PARKED_THRESH and not self.prev_parked:
-        self.prev_parked += 1
+      if blue_count > PARKED_THRESH:
+        self.prev_parked = 1
       elif self.prev_parked > PARKED_WAIT_FRAMES:
         self.prev_parked = 0
         self.parked_count += 1
@@ -128,8 +139,9 @@ class controller:
       elif self.prev_parked > 0:
         self.prev_parked += 1
       if self.parked_count >= PARKED_COUNT_END:
-        print('inner straight')
-        self.follow_state = INNER_STRAIGHT
+        print('inner turn')
+        self.prev_parked = 0
+        self.follow_state = INNER_TURN
       if zebra_count > ZEBRA_THRESH:
         self.send_vel(0, 0)
         print('ped')
@@ -145,7 +157,7 @@ class controller:
       if ped_count > PED_THRESH:
         self.prev_ped += 1
       elif self.prev_ped > 2:
-        if self.wait > ZEBRA_WAIT_FRAMES:
+        if self.wait > PED_WAIT_FRAMES:
           print('leave zebra')
           self.follow_state = LEAVE_ZEBRA
           self.wait = 0
@@ -160,17 +172,37 @@ class controller:
       if zebra_count < ZEBRA_THRESH:
         print('straight')
         self.follow_state = STRAIGHT
-      elif np.abs(right - LINE_FOLLOWX) > TURN_THRESH:
+      elif np.abs(LINE_FOLLOWX - right) > TURN_THRESH:
         self.send_vel(0, 1 if LINE_FOLLOWX - right > 0 else -1)
       else:
         self.send_vel(1, 0)
+    elif self.follow_state == INNER_TURN:
+      if self.wait >= INNER_TURN_MIN_FRAMES:
+        self.wait = 0
+        print('inner straight')
+        self.follow_state = INNER_STRAIGHT
+      else:
+        self.wait += 1
+      if INNER_FOLLOWX - left > TURN_THRESH:
+        self.send_vel(0, 1)
+      elif left > WIDTH/2:
+        self.send_vel(0, -1)
+      else:
+        self.send_vel(1, 0)
     elif self.follow_state == INNER_STRAIGHT:
-      self.send_vel(0, 0)
+      if right < WIDTH/6:
+        self.send_vel(1, 0)
+      elif np.abs(right - LINE_FOLLOWX) > TURN_THRESH:
+        self.send_vel(0, 1 if LINE_FOLLOWX - right > 0 else -1)
+      else:
+        self.send_vel(1, 0)  
     elif self.follow_state == STOP:
       self.send_vel(0, 0)
       self.follow_state += 1
 
-    cv2.imshow('cam', image_raw)
+    cv2.circle(white_mask, (int(right), SCAN_LINE), 20, (255, 0, 0), 2)
+
+    cv2.imshow('cam', white_mask)
     cv2.waitKey(1)
 
   def send_vel(self, lin, ang):
